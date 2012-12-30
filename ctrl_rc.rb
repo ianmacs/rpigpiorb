5a45fbbcdb2590e4bf2c8288cd5048da4f3c8ff9
ianmacs <ianmacs@m>
2012-12-30 11:13:50 +0100

    Reimplementation of RC car demo using GPIO class.

--- ctrl_rc.rb
+++ ctrl_rc.rb
@@ -1,34 +1,71 @@
 # Controls an RC car
-# This version does not use rpi_gpio
+require "rpi_gpio"
 
-RC = [
-B=24,
-F=23,
-R=22,
-L=17
-]
-
-def set(gpio,val)
-  system("echo #{val} >/sys/class/gpio/gpio#{gpio}/value")
+# operates an RC Car using 4 discrete GPIO pins like the RX-2B receiver chip does
+# It is important that backwards and forwards is never activated at the same time,
+# else the H-Bridge would short circuit the driving battery. Same for left and right.
+class RC_Car
+  # Initializer defines which GPIO pin is connected to which H-Bridge base.
+  def initialize(backwards_pin=24,
+                forwards_pin=23,
+                right_pin=22,
+                left_pin=17)
+    @backwards_pin = RPi_GPIO_Pin.new(backwards_pin,"out")
+    @forwards_pin = RPi_GPIO_Pin.new(forwards_pin,"out")
+    @right_pin = RPi_GPIO_Pin.new(right_pin,"out")
+    @left_pin = RPi_GPIO_Pin.new(left_pin,"out")
+    drive(0)
+    steer(0)
+  end
+  
+  # Drive :forwards, :backwards, or :stop.
+  # Only influences the drive motor, not the steering motor.
+  def drive(where)
+    case where
+    when :forward,:forwards
+      @backwards_pin.set(0)
+      @forwards_pin.set(1)
+    when :backwards,:backward
+      @forwards_pin.set(0)
+      @backwards_pin.set(1)
+    else
+      @forwards_pin.set(0)
+      @backwards_pin.set(0)
+    end
+  end
+  # Steer :right, :left, or :straight.
+  # Only influences the steering motor, not the driving motor.
+  def steer(where)
+    case where
+    when :left
+      @right_pin.set(0)
+      @left_pin.set(1)
+    when :right
+      @left_pin.set(0)
+      @right_pin.set(1)
+    else
+      @left_pin.set(0)
+      @right_pin.set(0)
+    end
+  end
 end
 
-RC.each {|gpio|
-  system("echo #{gpio} >/sys/class/gpio/export")
-  system("echo out >/sys/class/gpio/gpio#{gpio}/direction")
-  system("echo 0 >/sys/class/gpio/gpio#{gpio}/value")
-}
-loop do
-  set(F,1)
-  set(R,1)
+if __FILE__ == $0
+ rc = RC_Car.new
+
+ loop do
+  rc.drive(:forward)
+  rc.steer(:right)
   sleep(0.2)
-  set(F,0)
+  rc.drive(:stop)
   sleep(0.2)
-  set(R,0)
+  rc.steer(:straight)
   sleep(0.6)
-  set(B,1)
-  set(L,1)
+  rc.drive(:backward)
+  rc.steer(:left)
   sleep(0.2)
-  set(B,0)
-  set(L,0)
+  rc.drive(:stop)
+  rc.steer(:straight)
   sleep 1
+ end
 end
