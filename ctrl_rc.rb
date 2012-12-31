a58e138eac357baa6fc41d141428ea6d6bc5e90c
ianmacs <ianmacs@m>
2012-12-31 01:25:41 +0100

    Software pulse width modulation. Single control method. Timeout -> Stop

--- ctrl_rc.rb
+++ ctrl_rc.rb
@@ -2,6 +2,7 @@
 # Controls an RC car
 # This file is in the public domain. Share and enjoy.
 require "./rpi_gpio.rb"
+require "thread"
 
 # operates an RC Car using 4 discrete GPIO pins like the RX-2B receiver chip does
 # It is important that backwards and forwards is never activated at the same time,
@@ -59,22 +60,89 @@ class RC_Car
   end
 end
 
-if __FILE__ == $0
- rc = RC_Car.new
+# RC_Car that can be remote controlled by messages.
+# Constant messages are needed to keep car in motion.
+# Car will stop 1 second after latest invocation of control method.
+class RC_Car_Speed < RC_Car
+  def initialize(max_speed = 12, timeout = 1, *gpio_pins)
+    super(*gpio_pins)
+    # controls access to @alive, @timeout, @max_speed, @speed_counter, @speed, @turn
+    @mutex = Mutex.new 
+    @alive = Time.now
+    @timeout = timeout
+    @max_speed = max_speed
+    @speed_counter = 0
+    @speed = 0
+    @turn = :straight
+    @port = port
+    @interface = interface
+    @exit_threads = false;
+    @gpio_thread = Thread.new{control_gpio}
+    @udp_thread = Thread.new{receive_udp}
+  end
+  
+  # determines if the motor needs to be switched on or off to achieve the given speed
+  def motor_state(speed, max_speed, speed_counter)
+    last_time = (speed * 1.0 * (speed_counter-1) / max_speed).floor
+    this_time = (speed * 1.0 * speed_counter / max_speed).floor
+    return this_time != last_time
+  end
+  # inverts power_state
+  def motor_powersave(speed, max_speed, speed_counter)
+    return ! motor_state(speed, max_speed, speed_counter)
+  end
+  def control(max_speed, speed, turn)
+    @mutex.synchronize {
+      @max_speed = max_speed
+      @speed = speed
+      @turn = turn
+      @alive = Time.now
+    }
+  end
+  private
+  def control_gpio
+    loop {
+      sleep(0.001)
+      @mutex.lock
+      max_speed=@max_speed
+      speed=@speed
+      turn=@turn
+      speed_counter = @speed_counter
+      @speed_counter = (@speed_counter + 1) % @max_speed
+      timeout_occurred = (Time.now - @alive) > @timeout
+      @mutex.unlock
+      if timeout_occurred
+        driving = :stop
+        turn = :straight
+      else
+        if speed > 0
+          driving = :forward
+        else
+          driving = :backward
+        end
+        if motor_powersave(speed,max_speed,speed_counter)
+          driving = :stop
+        end
+      end
+      drive(driving)
+      steer(turn)
+    }
+  end
+end
 
- loop do
-  rc.drive(:forward)
-  rc.steer(:right)
-  sleep(0.2)
-  rc.drive(:stop)
-  sleep(0.2)
-  rc.steer(:straight)
-  sleep(0.6)
-  rc.drive(:backward)
-  rc.steer(:left)
-  sleep(0.2)
-  rc.drive(:stop)
-  rc.steer(:straight)
-  sleep 1
- end
+if __FILE__ == $0
+  rc = RC_Car_Speed.new
+
+  loop do
+    rc.control(12,+12,:right)
+    sleep(0.2)
+    rc.control(12,0,:right)
+    sleep(0.2)
+    rc.control(12,0,:straight)
+    sleep(0.6)
+    rc.control(12,-12,:left)
+    sleep(0.2)
+    rc.control(12,0,:straight)
+    sleep 1
+  end
 end
